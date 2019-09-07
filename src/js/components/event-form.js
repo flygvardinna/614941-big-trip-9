ab12eb12f18bd982b4fb70c85e94dd3f2fafa150
Оксана <zhuk11oxana@gmail.com>
2019-09-07 21:52:58 +0700

    fix error in event form markup

--- src/js/components/event-form.js
+++ src/js/components/event-form.js
@@ -23,21 +23,22 @@ const createOffersList = (offersList) => {
 };
 
 const getOfferTemplate = (offer) => {
-  return `<div class="event__available-offers">
-    <div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${offer.selected ? `checked` : ``}>
-      <label class="event__offer-label" for="event-offer-luggage-1">
-        <span class="event__offer-title">${offer.name}</span>
-        &plus;
-        &euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
-      </label>
-    </div>`;
+  return `<div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${offer.selected ? `checked` : ``}>
+    <label class="event__offer-label" for="event-offer-luggage-1">
+      <span class="event__offer-title">${offer.name}</span>
+      &plus;
+      &euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
+    </label>
+  </div>`;
 };
 
 const getAvailableOffersTemplate = (offersToRender) => {
   if (offersToRender.length > 0) {
     return `<section class="event__section  event__section--offers">
       <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+
+      <div class="event__available-offers">
       ${createOffersList(offersToRender)}
       </div>
     </section>`;
